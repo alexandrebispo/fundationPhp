fd389baa0996279e4cbe97c2901508a443ba10a1
alexandrebispo <alexandrebispo.mestre@gmail.com>
2014-07-15 22:53:00 -0300

    Commit do readme e do projeto fundation.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php require_once("includes/head.php"); ?>
+</head>
+
+<body role="document">
+<!-- Fixed navbar -->
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <?php require_once("includes/navbar.php"); ?>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
+
+        <div class="page-header">
+           <?php require_once("includes/conteudo.php"); ?>
+        </div>
+    </div> <!-- /container -->
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="dist/js/bootstrap.min.js"></script>
+<script src="assets/js/docs.min.js"></script>
+</body>
+</html>
