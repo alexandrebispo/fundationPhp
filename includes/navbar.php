f6f1ada917d0bc4f4896f271d07c6fd0ce0c2e14
alexandrebispo <alexandrebispo.mestre@gmail.com>
2014-07-18 23:30:40 -0300

    paginas e tratamento de rotas criadas e layout com bootstrap

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,23 +4,12 @@
     <span class="icon-bar"></span>
     <span class="icon-bar"></span>
 </button>
-<a class="navbar-brand" href="#">Bootstrap theme</a>
+<a class="navbar-brand" href="#">Fundation PHP</a>
 </div>
 <div class="navbar-collapse collapse">
     <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-            </ul>
-        </li>
+        <li class="active"><a href="<?php echo'index.php?pagina=home.php'; ?>">Home</a></li>
+        <li><a href="<?php echo'index.php?pagina=empresa.php'; ?>">Empresa</a></li>
+        <li><a href="<?php echo'index.php?pagina=produtos.php'; ?>">Produtos</a></li>
+        <li><a href="<?php echo'index.php?pagina=contato.php'; ?>">Contato</a></li>
     </ul>
--- index.php
+++ index.php
@@ -15,14 +15,27 @@
 </div>
 
         <div class="page-header">
-           <?php require_once("includes/conteudo.php"); ?>
+                <?php
+
+                if(isset($_GET['pagina']) && file_exists('includes/'.$_GET['pagina'])):
+                    require_once('includes/'.$_GET['pagina']);
+                else:   ?>
+                    <div class="alert alert-danger" role="alert">
+                        <strong>Aviso!</strong> esta página não existe.
+                    </div>
+
+               <?php
+                    require_once("includes/home.php");
+               endif;
+
+               ?>
         </div>
     </div> <!-- /container -->
 <!-- Bootstrap core JavaScript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="dist/js/bootstrap.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 <script src="assets/js/docs.min.js"></script>
 </body>
 </html>
